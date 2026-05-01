d412e4085240b04f39db2c3f41dc9488b1c94682
Anwesha <anwesha.birati@gmail.com>
2026-05-02 02:48:01 +0530

    cors working

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const passport = require("./config/passport");
 connectDB();
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5000'],
+  origin: ['http://localhost:3000', 'http://localhost:5000', 'https://figmaclone-delta.vercel.app/'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
